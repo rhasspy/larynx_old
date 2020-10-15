790d88795128303d323ff6264e9df88560135064
Michael Hansen <michael.hansen.24@us.af.mil>
2020-10-15 13:49:21 -0400

    Add MaryTTS-compatible endpoints

--- larynx/server.py
+++ larynx/server.py
@@ -29,27 +29,7 @@ def get_app(
         cache_dir = Path(cache_dir)
         cache_dir.mkdir(parents=True, exist_ok=True)
 
-    app = Flask("larynx", template_folder=str(_DIR / "templates"))
-    app.secret_key = str(uuid.uuid4())
-    CORS(app)
-
-    @app.route("/")
-    def app_index():
-        return render_template("index.html")
-
-    @app.route("/css/<path:filename>", methods=["GET"])
-    def css(filename) -> Response:
-        """CSS static endpoint."""
-        return send_from_directory("css", filename)
-
-    @app.route("/img/<path:filename>", methods=["GET"])
-    def img(filename) -> Response:
-        """Image static endpoint."""
-        return send_from_directory("img", filename)
-
-    @app.route("/api/tts", methods=["GET"])
-    def api_tts():
-        text = request.args.get("text")
+    def text_to_wav(text: str) -> bytes:
         _LOGGER.debug("Text: %s", text)
 
         wav_bytes: typing.Optional[bytes] = None
@@ -81,6 +61,52 @@ def get_app(
             if cached_wav_path:
                 cached_wav_path.write_bytes(wav_bytes)
 
+        return wav_bytes
+
+    # -------------------------------------------------------------------------
+
+    app = Flask("larynx", template_folder=str(_DIR / "templates"))
+    app.secret_key = str(uuid.uuid4())
+    CORS(app)
+
+    @app.route("/")
+    def app_index():
+        return render_template("index.html")
+
+    @app.route("/css/<path:filename>", methods=["GET"])
+    def css(filename) -> Response:
+        """CSS static endpoint."""
+        return send_from_directory("css", filename)
+
+    @app.route("/img/<path:filename>", methods=["GET"])
+    def img(filename) -> Response:
+        """Image static endpoint."""
+        return send_from_directory("img", filename)
+
+    @app.route("/api/tts", methods=["GET", "POST"])
+    def api_tts():
+        """Text to speech endpoint"""
+        if request.method == "POST":
+            text = request.data.encode()
+        else:
+            text = request.args.get("text")
+
+        wav_bytes = text_to_wav(text)
+
         return Response(wav_bytes, mimetype="audio/wav")
+
+    # MaryTTS compatibility layer
+    @app.route("/process", methods=["GET"])
+    def api_process():
+        """MaryTTS-compatible /process endpoint"""
+        text = request.args.get("INPUT_TEXT", "")
+        wav_bytes = text_to_wav(text)
+
+        return Response(wav_bytes, mimetype="audio/wav")
+
+    @app.route("/voices", methods=["GET"])
+    def api_voices():
+        """MaryTTS-compatible /voices endpoint"""
+        return "default\n"
 
     return app
