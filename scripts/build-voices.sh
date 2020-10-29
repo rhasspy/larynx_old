81a9ca6101a6d09e5f9740235e59134ac2417ca1
Michael Hansen <hansen.mike@gmail.com>
2020-10-29 16:05:06 -0400

    Bump versions

--- scripts/build-voices.sh
+++ scripts/build-voices.sh
@@ -16,57 +16,75 @@ mkdir -p "${download}"
 
 # -----------------------------------------------------------------------------
 
+temp_files=()
+function cleanup {
+    rm -f "${temp_files[@]}"
+}
+
+trap cleanup EXIT
+
+# -----------------------------------------------------------------------------
+
 voices_dir="${src_dir}/voices"
 
-find "${voices_dir}" -maxdepth 1 -mindepth 1 -type d | \
-    while read -r voice_dir; do
-        voice_name="$(basename "${voice_dir}")"
-        version="$(cat "${voice_dir}/VERSION")"
+voice_names="$(mktemp)"
+temp_files+=("${voice_names[@]}")
 
-        echo "Building voice ${voice_name} version ${version}..."
+if [[ -z "$1" ]]; then
+    # All voices
+    find "${voices_dir}" -maxdepth 1 -mindepth 1 -type d > "${voice_names}"
+else
+    # Specified voices
+    while [[ -n "$1" ]]; do
+        echo "${voices_dir}/$1" >> "${voice_names}"
+        shift
+    done
+fi
 
-        DOCKERFILE="${voice_dir}/Dockerfile"
+while read -r voice_dir; do
+    voice_name="$(basename "${voice_dir}")"
+    version="$(cat "${voice_dir}/VERSION")"
 
-        if [[ -n "${PROXY}" ]]; then
-            if [[ -z "${PROXY_IP}" ]]; then
-                export PROXY_IP="$(hostname -I | awk '{print $1}')"
-            fi
+    echo "Building voice ${voice_name} version ${version}..."
 
-            export PROXY_PORT=3142
-            export PROXY="${PROXY_IP}:${PROXY_PORT}"
-            export PYPI_PORT=4000
-            export PYPI="${PROXY_IP}:${PYPI_PORT}"
-            export PYPI_HOST="${PROXY_IP}"
+    DOCKERFILE="${voice_dir}/Dockerfile"
 
-            # Use temporary file instead
-            temp_dockerfile="$(mktemp -p "${voice_dir}")"
-            function cleanup {
-                rm -f "${temp_dockerfile}"
-            }
-
-            trap cleanup EXIT
-
-            # Run through pre-processor to replace variables
-            "${src_dir}/docker/preprocess.sh" < "${DOCKERFILE}" > "${temp_dockerfile}"
-            DOCKERFILE="${temp_dockerfile}"
+    if [[ -n "${PROXY}" ]]; then
+        if [[ -z "${PROXY_IP}" ]]; then
+            export PROXY_IP="$(hostname -I | awk '{print $1}')"
         fi
 
-        if [[ -n "${DOCKER_BUILDX}" ]]; then
-            docker buildx build \
-                   "${src_dir}" \
-                   -f "${DOCKERFILE}" \
-                   "--platform=${PLATFORMS}" \
-                   --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
-                   --tag "${DOCKER_REGISTRY}/rhasspy/larynx:${voice_name}-${version}" \
-                   --push \
-                   "$@"
-        else
-            docker build \
-                   "${src_dir}" \
-                   -f "${DOCKERFILE}" \
-                   --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
-                   --tag "${DOCKER_REGISTRY}/rhasspy/larynx:${voice_name}-${version}" \
-                   "$@"
-        fi
+        export PROXY_PORT=3142
+        export PROXY="${PROXY_IP}:${PROXY_PORT}"
+        export PYPI_PORT=4000
+        export PYPI="${PROXY_IP}:${PYPI_PORT}"
+        export PYPI_HOST="${PROXY_IP}"
 
-    done # for each voice directory
+        # Use temporary file instead
+        temp_dockerfile="$(mktemp -p "${voice_dir}")"
+        temp_files+=("${temp_dockerfile}")
+
+        # Run through pre-processor to replace variables
+        "${src_dir}/docker/preprocess.sh" < "${DOCKERFILE}" > "${temp_dockerfile}"
+        DOCKERFILE="${temp_dockerfile}"
+    fi
+
+    if [[ -n "${DOCKER_BUILDX}" ]]; then
+        docker buildx build \
+                "${src_dir}" \
+                -f "${DOCKERFILE}" \
+                "--platform=${PLATFORMS}" \
+                --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
+                --tag "${DOCKER_REGISTRY}/rhasspy/larynx:${voice_name}-${version}" \
+                --push \
+                "$@"
+    else
+        docker build \
+                "${src_dir}" \
+                -f "${DOCKERFILE}" \
+                --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
+                --tag "${DOCKER_REGISTRY}/rhasspy/larynx:${voice_name}-${version}" \
+                "$@"
+    fi
+
+done < "${voice_names}"
